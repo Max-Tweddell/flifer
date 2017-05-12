42f99aff9c9d29e8f7ca1edf341a9bd3d110b891
Max Tweddell <maxt.nz@gmail.com>
2017-05-12 19:45:45 +1200

    added working code and example image"

--- src/flifer/core.clj
+++ src/flifer/core.clj
@@ -1,12 +1,32 @@
 (ns flifer.core
-  (:require [clojure.java.shell :as shell ])
+  (:require
+   [clojure.java.shell :as shell ]
+   [clojure.string :as str ]
+            )
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (shell/sh "" "-aul"))
+  (multi-thread))
 
 (defn convert [image]
-(shell/sh flif -e image )
+  (shell/sh "flif"  "-e"  "-E 100" (str  image ".png") (str image ".flif") :dir "images" )
+  )
+(defn image-split [image]
+  (convert (first (str/split (first  (str/split image #"\.")) #"\n"))))
+
+(defn get-images []
+  (map image-split
+       (str/split (:out (shell/sh "ls" :dir "images")) #"\n")))
+
+(defn multi-thread []
+  (do
+    (get-images)
+    (get-images)
+    (get-images)
+    (get-images)
+    (get-images)
+    (get-images)
+    (get-images))
   )
